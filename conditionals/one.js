6161518dac3d9f19916991756be384220c741359
Celeste-Rhoades <celesteisthebest82@gmail.com>
2026-01-28 15:11:23 -0700

    progress

--- conditionals/one.js
+++ conditionals/one.js
@@ -6,11 +6,11 @@ if (qty > 30 && qty % 5 === 4) {
 } else {
   console.log("swoosh");
 }
-
+//swoosh
 if (qty > 0) {
   console.log("pos");
 }
-
+//pos
 // snippet 1-1
 let a = "celery";
 let b = "SQUASH";
@@ -18,10 +18,13 @@ let b = "SQUASH";
 if (a === a.toUpperCase()) {
   console.log("alpha");
 }
+//none
 
 if (b === b.toUpperCase()) {
   console.log("beta");
 }
+//beta
+
 // snippet 1-2
 let number = 9;
 
@@ -30,14 +33,15 @@ if (number > 4) {
 } else if (number % 3 === 0) {
   console.log("dong");
 }
-
+//ding
 // snippet 1-3
 let z = 12;
 
 if (z > 10) {
   console.log("vroom");
 }
-
+//vroom
 if (z % 3 === 0) {
   console.log("skrrt");
 }
+//skrrt
