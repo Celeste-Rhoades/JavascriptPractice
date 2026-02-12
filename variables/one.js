859d357794bdcd21d0f44a557708bdb2e1d09820
Celeste-Rhoades <celesteisthebest82@gmail.com>
2026-02-11 19:35:26 -0700

    progress

--- variables/one.js
+++ variables/one.js
@@ -1,12 +1,12 @@
 let word = "bye";
-console.log(word + " felicia");
-console.log(word);
+console.log(word + " felicia"); //bye felcia
+console.log(word); //bye
 
 let num = 10;
-num = num * 2;
-console.log(num);
+num = num * 2; //20
+console.log(num); //20
 
 let bottlesOfBeer = 99;
 let around = bottlesOfBeer - 1;
-console.log(around);
-console.log(bottlesOfBeer);
+console.log(around); //98
+console.log(bottlesOfBeer); //99
